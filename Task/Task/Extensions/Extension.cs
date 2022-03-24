a4a9c5e33a124775e6f5545a579e4169feda5db9
Ilkin Yusubov <tu758403c@code.edu.az>
2022-03-24 11:53:27 +0400

    add Extension

---
+++ Task/Task/Extensions/Extension.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task.Extensions
+{
+    static class Extension
+    {
+
+
+        public static string customContains(this string word,string findword)
+        {
+            
+                StringBuilder stringBuilder = new StringBuilder();
+                word = word.ToLower();
+                string[] words = word.Split();
+                
+
+            foreach (string word1 in words)
+            {
+                stringBuilder.Append(char.ToUpper(word[0]));
+                for (int i = 1; i < word.Length; i++)
+                {
+                    stringBuilder.Append(word[i]);
+                }
+
+
+                if (word != words[words.Length - 1])
+                {
+                    stringBuilder.Append(" ");
+                }
+
+            }
+
+
+            return stringBuilder.ToString();
+
+
+
+        }
+        
+
+
+    }
+}
---
+++ Task/Task/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string word = "Bu bir custom contains methodudur";
+            
+
+            Console.WriteLine(word.Contains("bu"));
+            
+
+
+
+        }
+
+        
+    }
+}
